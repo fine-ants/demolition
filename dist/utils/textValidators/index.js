41a170f23256089f3eb6d1e8d55381daeb500631
Jay <chlwodnjs34@gmail.com>
2024-03-06 16:36:05 +0900

    fix: Fix incorrect export

--- dist/utils/textValidators/index.js
+++ dist/utils/textValidators/index.js
@@ -3,9 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.validatePassword = exports.validateNickname = exports.validateEmail = exports.getElapsedSince = void 0;
-var getElapsedSince_1 = require("../date/getElapsedSince/getElapsedSince");
-Object.defineProperty(exports, "getElapsedSince", { enumerable: true, get: function () { return __importDefault(getElapsedSince_1).default; } });
+exports.validatePassword = exports.validateNickname = exports.validateEmail = void 0;
 var validateEmail_1 = require("./validateEmail");
 Object.defineProperty(exports, "validateEmail", { enumerable: true, get: function () { return __importDefault(validateEmail_1).default; } });
 var validateNickname_1 = require("./validateNickname/validateNickname");
--- packages/utils/textValidators/index.ts
+++ packages/utils/textValidators/index.ts
@@ -1,4 +1,3 @@
-export { default as getElapsedSince } from "../date/getElapsedSince/getElapsedSince";
 export { default as validateEmail } from "./validateEmail";
 export { default as validateNickname } from "./validateNickname/validateNickname";
 export { default as validatePassword } from "./validatePassword/validatePassword";
