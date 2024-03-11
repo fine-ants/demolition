8e2c113d5c0851a01b28216bc5f6ca6d1f7ba8f5
Daeram Chung <daeram.chung@gmail.com>
2024-03-11 18:13:33 +0900

    1.1.9

--- dist/utils/date/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { default as getElapsedSince } from "./getElapsedSince/getElapsedSince";
--- dist/utils/date/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getElapsedSince = void 0;
-var getElapsedSince_1 = require("./getElapsedSince/getElapsedSince");
-Object.defineProperty(exports, "getElapsedSince", { enumerable: true, get: function () { return __importDefault(getElapsedSince_1).default; } });
-//# sourceMappingURL=index.js.map
