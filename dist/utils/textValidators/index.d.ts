5c5bd49a9851572b2789df0412eb7408d5087bb3
Daeram Chung <daeram.chung@gmail.com>
2024-03-11 17:01:50 +0900

    #8 chore: Git ignore dist

--- dist/utils/textValidators/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as validateEmail } from "./validateEmail";
-export { default as validateNickname } from "./validateNickname/validateNickname";
-export { default as validatePassword } from "./validatePassword/validatePassword";
