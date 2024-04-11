e5879fa2e4fbb9062f266011addfecc659cfb586
Jay <chlwodnjs34@gmail.com>
2024-04-11 17:15:09 +0900

    #14 refactor : Modify JSDoc for handleNumericCallback

--- packages/utils/number/handleNumericCallback/handleNumericCallback.ts
+++ packages/utils/number/handleNumericCallback/handleNumericCallback.ts
@@ -4,11 +4,11 @@ import {
 } from "../thousandsDelimiter/thousandsDelimiter";
 
 /**
- * This function if the value is empty string or numerical, it calls the callback with the value as an argument.
+ * This function executes the provided callback using the provided value if the value is an empty string or number.
  *
  * Useful for ensuring numerical input in onChange events.
  *
- * @param {string} value - The input value to be processed.
+ * @param {string} value - The input value to be processed by the callback function.
  * @param {(value: string) => void} callback - A callback function receiving the value only if it's numeric or an empty string.
  * @param {boolean} [commas=true] - A boolean flag to include thousands delimiter(comma) in the formatted value. (default: true).
  */
