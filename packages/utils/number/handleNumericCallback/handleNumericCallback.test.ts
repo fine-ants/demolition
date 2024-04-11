1d4683fe1bf756777301b33c6753a61b9bf14a69
Jay <chlwodnjs34@gmail.com>
2024-04-11 18:11:08 +0900

    #14 refactor : Modify parameter - Add delimiter - Modify commas -> shouldDelimiter

--- packages/utils/number/handleNumericCallback/handleNumericCallback.test.ts
+++ packages/utils/number/handleNumericCallback/handleNumericCallback.test.ts
@@ -8,22 +8,39 @@ describe("handleNumericCallback util function", () => {
   });
 
   it("should call the callback with an empty string if the input value is an empty string", () => {
-    handleNumericCallback("", mockCallback);
+    handleNumericCallback({
+      value: "",
+      callback: mockCallback,
+    });
     expect(mockCallback).toHaveBeenCalledWith("");
   });
 
   it("should call the callback with the formatted value if the input value is a number", () => {
-    handleNumericCallback("1234", mockCallback);
+    handleNumericCallback({ value: "1234", callback: mockCallback });
     expect(mockCallback).toHaveBeenCalledWith("1,234");
   });
 
   it("should call the callback with the unformatted value if the input value is a number and commas are disabled", () => {
-    handleNumericCallback("1234", mockCallback, false);
+    handleNumericCallback({
+      value: "1234",
+      callback: mockCallback,
+      shouldDelimiter: false,
+    });
     expect(mockCallback).toHaveBeenCalledWith("1234");
   });
 
-  test("should not call the callback if the input value is not a number", () => {
-    handleNumericCallback("123hi", mockCallback);
+  it("should not call the callback if the input value is not a number", () => {
+    handleNumericCallback({ value: "123hi", callback: mockCallback });
     expect(mockCallback).not.toHaveBeenCalled();
+  });
+
+  it("should call the callback with the formatted value using a custom delimiter", () => {
+    handleNumericCallback({
+      value: "1234",
+      callback: mockCallback,
+      shouldDelimiter: true,
+      delimiter: "-",
+    });
+    expect(mockCallback).toHaveBeenCalledWith("1-234");
   });
 });
--- packages/utils/number/handleNumericCallback/handleNumericCallback.ts
+++ packages/utils/number/handleNumericCallback/handleNumericCallback.ts
@@ -3,25 +3,38 @@ import {
   thousandsDelimiter,
 } from "../thousandsDelimiter/thousandsDelimiter";
 
+type Props = {
+  value: string;
+  callback: (value: string) => void;
+  shouldDelimiter?: boolean;
+  delimiter?: string;
+};
+
 /**
  * This function executes the provided callback using the provided value if the value is an empty string or number.
  *
  * Useful for ensuring numerical input in onChange events.
  *
- * @param {string} value - The input value to be processed by the callback function.
- * @param {(value: string) => void} callback - A callback function receiving the value only if it's numeric or an empty string.
- * @param {boolean} [commas=true] - A boolean flag to include thousands delimiter(comma) in the formatted value. (default: true).
- */
-export function handleNumericCallback(
-  value: string,
-  callback: (value: string) => void,
-  commas: boolean = true
-) {
-  const parsedValue = removeThousandsDelimiter(value);
+ * @param {Props} props - The properties object.
+ * @param {string} props.value - The input value to be processed by the callback function.
+ * @param {(value: string) => void} props.callback - A callback function receiving the value only if it's numeric or an empty string.
+ * @param {boolean} [props.shouldDelimiter=true] - A boolean flag to include thousands delimiter in the formatted value. (default: true)
+ * @param {string} [props.delimiter=","] - The thousands delimiter to use. (default: ",")
+ * **/
+
+export function handleNumericCallback({
+  value,
+  callback,
+  shouldDelimiter = true,
+  delimiter = ",",
+}: Props) {
+  const parsedValue = removeThousandsDelimiter(value, delimiter);
 
   if (parsedValue === "") {
     callback(parsedValue);
   } else if (!isNaN(Number(parsedValue))) {
-    callback(commas ? thousandsDelimiter(parsedValue) : parsedValue);
+    callback(
+      shouldDelimiter ? thousandsDelimiter(parsedValue, delimiter) : parsedValue
+    );
   }
 }
