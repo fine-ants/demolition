a2ca4c38fef1657b39f6e5a7ca1647137ebff648
Daeram Chung <daeram.chung@gmail.com>
2024-05-07 14:13:31 +0900

    fix: Export useNumber

--- packages/hooks/index.ts
+++ packages/hooks/index.ts
@@ -1,4 +1,5 @@
 export { default as useDebounce } from "./useDebounce/useDebounce";
 export { default as useFunnel } from "./useFunnel/useFunnel";
 export { default as useImageInput } from "./useImageInput/useImageInput";
+export { default as useNumber } from "./useNumber/useNumber";
 export { default as useText } from "./useText/useText";
